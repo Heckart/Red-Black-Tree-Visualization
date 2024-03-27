7a3ae701dcb4245cc0dc40a66f6dc49336cbbb3d
Ethan Heckart <ewheck3@gmail.com>
2024-03-26 20:30:35 -0500

    Implemented rotation & insertion functions
    Implemented insertion and rotation functions. Also added POSIX compliant new
    lines to the end of each file, resturctured the queue implementation to be in the
    Red_Black_Tree files, standardized the pointer syntax across function declarations,
    and implemented the initializeTree() and createNode() functions.

--- Red_Black_Tree.c
+++ Red_Black_Tree.c
@@ -1,5 +1,201 @@
 #include "Red_Black_Tree.h"
+
 #include "stdio.h"
+#include "stdlib.h"
+// Initialization
+redBlackTree* initializeTree()
+{
+    redBlackTree* tree = (redBlackTree*)malloc(sizeof(redBlackTree));
+    if (tree == NULL) // if there is a memory allocation failure
+    {
+        return NULL;
+    }
+    tree->root = NULL;
+    return tree;
+}
+
+
+
+// Node management
+treeNode* createNode(int data)
+{
+    treeNode* newNode = (treeNode*)malloc(sizeof(treeNode));
+    if (newNode == NULL)
+    {
+        return NULL;
+    }
+    
+    newNode->data = data;
+    newNode->color = RED; // new nodes are typically red by convention
+    newNode->left = NULL;
+    newNode->right = NULL;
+    newNode->parent = NULL;
+    
+    return newNode;
+}
+
+
+
+// Insertion fuctions
+void insert(redBlackTree* tree, int data)
+{
+    treeNode* newNode = createNode(data);
+    if (tree->root == NULL)
+    {
+        tree->root = newNode;
+    } else
+    {
+        treeNode* current = tree->root;
+        treeNode* parent = NULL;
+        while (current != NULL)
+        {
+            parent = current;
+            if (newNode->data < current->data)
+            {
+                current = current->left;
+            } else
+            {
+                current = current->right;
+            }
+        }
+        newNode->parent = parent;
+        if (newNode->data < parent->data)
+        {
+            parent->left = newNode;
+        } else
+        {
+            parent->right = newNode;
+        }   
+    }
+    
+    newNode->color = RED;
+    fixViolationsAfterInsertion(tree, newNode);
+}
+
+void fixViolationsAfterInsertion(redBlackTree* tree, treeNode* node)
+{
+    treeNode* parent = NULL;
+    treeNode* grandparent = NULL;
+    while ((node != tree->root) && (node->color != BLACK) && (node->parent->color == RED))
+    {
+        parent = node->parent;
+        grandparent = node->parent->parent;
+
+        // if parent is left child of grandparent
+        if (parent == grandparent->left)
+        {
+            treeNode* uncle = grandparent->right;
+            // if uncle of node is also red
+            if (uncle != NULL && uncle->color == RED)
+            {
+                grandparent->color = RED;
+                parent->color = BLACK;
+                uncle->color = BLACK;
+                node = grandparent;
+            } else
+            {
+                // Left-right
+                if (node == parent->right)
+                {
+                    leftRotate(&tree->root, parent);
+                    node = parent;
+                    parent = node->parent;
+                }
+                // Left-left
+                rightRotate(&tree->root, grandparent);
+                Color tempColor = parent->color;
+                parent->color = grandparent->color;
+                grandparent->color = tempColor;
+                node = parent;
+            }
+        } else
+        {
+            treeNode* uncle = grandparent->left;
+            // if uncle is red, only recolor
+            if (uncle != NULL && uncle->color == RED)
+            {
+                grandparent->color = RED;
+                parent->color = BLACK;
+                uncle->color = BLACK;
+                node = grandparent;
+            } else
+            {
+                // Right-left
+                if (node == parent->left)
+                {
+                    rightRotate(&tree->root, parent);
+                    node = parent;
+                    parent = node->parent;
+                }
+                // Right-right
+                leftRotate(&tree->root, grandparent);
+                Color tempColor = parent->color;
+                parent->color = grandparent->color;
+                grandparent->color = tempColor;
+                node = parent;
+            }
+        }  
+    }
+}
+
+// Rotation functions
+void leftRotate(treeNode** root, treeNode* x)
+{
+    // set y and turn y's left subtree into x's right subtree
+    treeNode *y = x->right;
+    x->right = y->left;
+
+    if (y->left != NULL)
+    {
+        y->left->parent = x;
+    }
+    
+    y->parent = x->parent; // link x's parent to y
+
+    if (x->parent == NULL) // if x is root
+    {
+        *root = y;
+    } else if (x == x->parent->left) // if x is left child
+    {
+        x->parent->left = y;
+    } else // if x is right child
+    {
+        x->parent->right = y;
+    }
+    
+    y->left = x;
+    x->parent = y;   
+}
+
+void rightRotate(treeNode** root, treeNode* y)
+{
+    // set x and turn x's left subtree into y's rught subtree
+    treeNode* x = y->left;
+    y->left = x->right;
+
+    if (x->right != NULL)
+    {
+        x->right->parent = y;
+    }
+
+    x->parent = y->parent; // link y's parent to x
+
+    if (y->parent == NULL) // if y is root
+    {
+        *root = x;
+    } else if (y == y->parent->left) // if y is left child
+    {
+        y->parent->right = x;
+    } else // if y is right child
+    {
+        y->parent->right = x;
+    }
+    
+   x->parent = y;
+   y->parent = x; 
+}
+
+
 
 // Traversal functions
 void inOrderTraversal(const treeNode* root)
@@ -73,6 +269,8 @@ void levelOrderTraversal(const treeNode* root)
     
     free(q); // all nodes need to be dequeud to prevent memory leaks
 }
+
+
 
 // Utility functions
 treeNode* getSuccessor(const treeNode* node)
@@ -167,4 +365,55 @@ bool isEmpty(const redBlackTree *tree)
     {
         return true;
     } else return false;
+}
+
+
+
+// Queue functions
+Queue* createQueue()
+{
+    Queue* q = (Queue*)malloc(sizeof(Queue));
+    q->front = q->rear = NULL;
+    return q;
+}
+
+void enqueue(Queue* q, const treeNode* treeNode)
+{
+    QueueNode* newNode = (QueueNode*)malloc(sizeof(QueueNode));
+    newNode->treeNode = treeNode;
+    newNode->next = NULL;
+
+    if (q->rear == NULL)
+    {
+        q->front = q->rear = newNode;
+        return;
+    }
+
+    q->rear->next = newNode;
+    q->rear = newNode;
+}
+
+const treeNode* dequeue(Queue* q)
+{
+    if (q->front == NULL)
+    {
+        return NULL;
+    }
+
+    QueueNode* temp = q->front;
+    const treeNode* treeNode = temp->treeNode;
+    q->front = q->front->next;
+
+    if (q->front == NULL)
+    {
+        q->rear = NULL;
+    }
+    
+    free(temp);
+    return treeNode;
+}
+
+bool isQueueEmpty(Queue* q)
+{
+    return q->front == NULL;
 }
--- Red_Black_Tree.h
+++ Red_Black_Tree.h
@@ -1,8 +1,6 @@
 #ifndef RED_BLACK_TREE
 #define RED_BLACK_TREE
 
-#include "Dynamic_Queue.h"
-
 #include "stdbool.h"
 #include "stddef.h"
 
@@ -12,14 +10,14 @@ typedef struct treeNode
 {
     int data;
     Color color;
-    struct treeNode *left;
-    struct treeNode *right;
-    struct treeNode *parent;
+    struct treeNode* left;
+    struct treeNode* right;
+    struct treeNode* parent;
 } treeNode;
 
 typedef struct redBlackTree
 {
-    treeNode *root;
+    treeNode* root;
     
     /*
     * having a redBlackTree struct allows us to store more metadata
@@ -27,6 +25,40 @@ typedef struct redBlackTree
     * such as a treeSize variable
     */
 } redBlackTree;
+
+
+// Initialization
+
+// set up an empty tree
+redBlackTree* initializeTree();
+
+
+
+// Node management
+
+// kind of stunted...just creates a new node with given data
+treeNode* createNode(int data);
+
+
+
+// Insertion
+
+// insert a new node into the tree
+void insert(redBlackTree* tree, int data);
+
+// fix color and strucutral violations after insertion
+void fixViolationsAfterInsertion(redBlackTree* tree, treeNode* node);
+
+
+
+// Rotation functions (should only be used as a part of insert & delete functions)
+
+// performs a left rotation on the given node
+void leftRotate(treeNode** root, treeNode* x);
+
+// performs a right rotation on the given node
+void rightRotate(treeNode** root, treeNode* y);
+
 
 
 // Traversal functions
@@ -43,26 +75,58 @@ void postOrderTraversal(const treeNode* root);
 // traverses the tree level by level (esssentially a BFS). Uses a queue under the hood.
 void levelOrderTraversal(const treeNode* root);
 
+
+
 // Utility functions
 
 // returns the in-order successor of the given node
-treeNode* getSuccessor(const treeNode *node);
+treeNode* getSuccessor(const treeNode* node);
 
 // returns the in-order predecessor of the given node
-treeNode* getPredecessor(const treeNode *node);
+treeNode* getPredecessor(const treeNode* node);
 
 // Returns true if the given node is red.
-bool isRed(const treeNode *node);
+bool isRed(const treeNode* node);
 
 // Returns true if the given node is black.
-bool isBlack(const treeNode *node);
+bool isBlack(const treeNode* node);
 
 // Returns the color of the given node.
-Color getColor(const treeNode *node);
+Color getColor(const treeNode* node);
+
+
 
 // Additional features
 
 // Returns true if tree is empty
-bool isEmpty(const redBlackTree *tree);
+bool isEmpty(const redBlackTree* tree);
+
+
+
+// Queue functions
+
+typedef struct QueueNode 
+{
+    const treeNode* treeNode;
+    struct QueueNode* next;
+} QueueNode;
+
+typedef struct Queue 
+{
+    QueueNode* front;
+    QueueNode* rear;
+} Queue;
+
+// initialize a queue
+Queue* createQueue();
+
+// enqueue a treeNode
+void enqueue(Queue* q, const treeNode* treeNode);
+
+// dequeue a treeNode and return it
+const treeNode* dequeue(Queue* q);
+
+// check if the queue is empty
+bool isQueueEmpty(Queue* q);
 
 #endif
